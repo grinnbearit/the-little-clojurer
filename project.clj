7350981d153a56bcf9adfb58874b88f3dfa9a9d7
Sidhant Godiwala <sidhant.godiwala@gmail.com>
2011-07-11 16:58:58 +0530

    first commit

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject the-little-clojurer "0.1.0"
+  :description "The Little Schemer in Clojure"
+  :dependencies [[org.clojure/clojure "1.2.1"]]
+  :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT"]])
